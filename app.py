ce0360efa946422dd843c2f80fd8a4d9f27b6fc7
Nirjhar Debnath <nirjhar@Nirjhars-MacBook-Air.local>
2020-04-26 16:57:36 +0530

    first commit

---
+++ app.py
@@ -0,0 +1,24 @@
+from flask import Flask,render_template,url_for,request
+import pandas as pd 
+import pickle
+
+
+app = Flask(__name__)
+
+model = pickle.load(open('model.pkl','rb'))
+
+@app.route('/')
+def home():
+	return render_template('home.html')
+
+@app.route('/predict',methods=['POST'])
+def predict():
+	comment = request.form['comment']
+	data = [comment]
+	my_prediction = model.predict(data)
+	return render_template('result.html',prediction = my_prediction)
+
+
+
+if __name__ == '__main__':
+	app.run(debug=True)
